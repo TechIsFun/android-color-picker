68cdb8d60d3c433408550e78e1ca710a9f2a9ca5
Andrea <andrea.maglie@gmail.com>
2013-12-26 16:57:47 +0100

    robolectric setup

---
+++ ColorPicker/src/test/java/com/github/techisfun/android/colorpicker/ColorPickerFragmentTest.java
@@ -0,0 +1,21 @@
+package com.github.techisfun.android.colorpicker;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class ColorPickerFragmentTest {
+
+    @Test
+    public void shouldFail() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void shouldPass() {
+        assertTrue(true);
+    }
+
+
+}
